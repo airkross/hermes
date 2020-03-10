ac114adc0d8839709a4c2a99acdcd984f2366916
Ruslan <40143122+airkross@users.noreply.github.com>
2020-03-11 00:36:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -29,76 +29,91 @@ for (var i = 0; i < acLiColl.length; i++) {
 }
 var catPageContent = document.querySelector('.cat-page-content');
 
+var url = window.location.toString();
+url = url.split('/');
+if (url[url.length - 1] == 'solutions.html') {
+    for (var i = 0; i < acLiColl.length; i++) {
+        acLiColl[i].addEventListener('click', function () {
+            while (catPageContent.firstChild) {
+                catPageContent.removeChild(catPageContent.firstChild);
+            }
+            var acSubItem = this.querySelectorAll('.ac-sub-item');
+            for (var j = 0; j < acSubItem.length; j++) {
+                var catPageCard = document.createElement('div'),
+                    catPageCardTitle = document.createElement('div'),
+                    catPageCardBodyBig = document.createElement('div'),
+                    catPageCardBigImg = document.createElement('div'),
+                    catPageCardBigDiscr = document.createElement('div'),
+                    img = document.createElement('img');
+                img.src = 'img/s.png';
+                catPageCardBodyBig.className = 'cat-page-card-body-big';
+                catPageCardBigImg.className = 'cat-page-card-big-img';
+                catPageCardBigDiscr.className = 'cat-page-card-big-discr';
+                catPageCard.className = 'cat-page-card';
+                catPageCardTitle.className = 'cat-page-card-title';
+                catPageCardTitle.innerHTML = acSubItem[j].innerHTML;
+                catPageCardBigDiscr.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
 
+                catPageCardBigImg.appendChild(img);
+                catPageCardBodyBig.appendChild(catPageCardBigImg);
+                catPageCardBodyBig.appendChild(catPageCardBigDiscr);
 
-// catPageCardBodyImg.innerHTML = 'sdsdsd';
-for (var i = 0; i < acLiColl.length; i++) {
+                catPageCard.appendChild(catPageCardTitle);
+                catPageCard.appendChild(catPageCardBodyBig);
+                catPageContent.appendChild(catPageCard);
+            }
+        });
+    }
+} else if (url[url.length - 1] == 'catalog-page.html') {
+    for (var i = 0; i < acLiColl.length; i++) {
 
-    acLiColl[i].addEventListener('click', function () {
-        while (catPageContent.firstChild) {
-            catPageContent.removeChild(catPageContent.firstChild);
-        }
-        var acSubItem = this.querySelectorAll('.ac-sub-item');
-        for (var j = 0; j < acSubItem.length; j++) {
-            var catPageCard = document.createElement('div'),
-                catPageCardTitle = document.createElement('div'),
-                catPageCardBody = document.createElement('div'),
-                catPageCardBodyImg = document.createElement('div'),
-                catCageCardBodyContent = document.createElement('div'),
-                catPageCardBodyContentTitle = document.createElement('div'),
-                catPageCardBodyContentDiscr = document.createElement('div'),
-                catPageCardBodyContentBtn = document.createElement('div'),
-                btn = document.createElement('button'),
-                img = document.createElement('img');
-            img.src = 'img/bgi.png';
-            catPageCard.className = 'cat-page-card';
-            catPageCardTitle.className = 'cat-page-card-title';
-            catPageCardBody.className = 'cat-page-card-body';
-            catCageCardBodyContent.className = 'cat-page-card-body-content';
-            catPageCardBodyContentTitle.className = 'cat-page-card-body-content-title';
-            catPageCardBodyContentDiscr.className = 'cat-page-card-body-content-discr';
-            catPageCardBodyContentBtn.className = 'cat-page-card-body-content-btn';
-            btn.className = 'cat-page-card-btn-btn';
-            catPageCardBodyImg.className = 'cat-page-card-body-img';
-            // console.log(acSubItem[j].innerHTML);
-            catPageCardTitle.innerHTML = acSubItem[j].innerHTML;
-            catPageCardBodyContentTitle.innerHTML = acSubItem[j].innerHTML;
-            btn.innerHTML = 'Подробнее';
-            catPageCardBodyContentDiscr.innerHTML = 'Описание';
+        acLiColl[i].addEventListener('click', function () {
+            while (catPageContent.firstChild) {
+                catPageContent.removeChild(catPageContent.firstChild);
+            }
+            var acSubItem = this.querySelectorAll('.ac-sub-item');
+            for (var j = 0; j < acSubItem.length; j++) {
+                var catPageCard = document.createElement('div'),
+                    catPageCardTitle = document.createElement('div'),
+                    catPageCardBody = document.createElement('div'),
+                    catPageCardBodyImg = document.createElement('div'),
+                    catCageCardBodyContent = document.createElement('div'),
+                    catPageCardBodyContentTitle = document.createElement('div'),
+                    catPageCardBodyContentDiscr = document.createElement('div'),
+                    catPageCardBodyContentBtn = document.createElement('div'),
+                    btn = document.createElement('button'),
+                    img = document.createElement('img');
+                img.src = 'img/bgi.png';
+                catPageCard.className = 'cat-page-card';
+                catPageCardTitle.className = 'cat-page-card-title';
+                catPageCardBody.className = 'cat-page-card-body';
+                catCageCardBodyContent.className = 'cat-page-card-body-content';
+                catPageCardBodyContentTitle.className = 'cat-page-card-body-content-title';
+                catPageCardBodyContentDiscr.className = 'cat-page-card-body-content-discr';
+                catPageCardBodyContentBtn.className = 'cat-page-card-body-content-btn';
+                btn.className = 'cat-page-card-btn-btn';
+                catPageCardBodyImg.className = 'cat-page-card-body-img';
+                catPageCardTitle.innerHTML = acSubItem[j].innerHTML;
+                catPageCardBodyContentTitle.innerHTML = acSubItem[j].innerHTML;
+                btn.innerHTML = 'Подробнее';
+                catPageCardBodyContentDiscr.innerHTML = 'Описание';
 
-            // append
-            catPageCardBodyContentBtn.appendChild(btn);
+                // append
+                catPageCardBodyContentBtn.appendChild(btn);
 
-            catCageCardBodyContent.appendChild(catPageCardBodyContentTitle);
-            catCageCardBodyContent.appendChild(catPageCardBodyContentDiscr);
-            catCageCardBodyContent.appendChild(catPageCardBodyContentBtn);
+                catCageCardBodyContent.appendChild(catPageCardBodyContentTitle);
+                catCageCardBodyContent.appendChild(catPageCardBodyContentDiscr);
+                catCageCardBodyContent.appendChild(catPageCardBodyContentBtn);
 
-            catPageCardBodyImg.appendChild(img);
+                catPageCardBodyImg.appendChild(img);
 
-            catPageCardBody.appendChild(catPageCardBodyImg);
-            catPageCardBody.appendChild(catCageCardBodyContent);
+                catPageCardBody.appendChild(catPageCardBodyImg);
+                catPageCardBody.appendChild(catCageCardBodyContent);
 
-            catPageCard.appendChild(catPageCardTitle);
-            catPageCard.appendChild(catPageCardBody);
-            catPageContent.appendChild(catPageCard);
-        }
-
-
-
-
-
-
-        // var collSubTags = this.querySelectorAll('.ac-sub-item');
-
-        // for (var j = 0; j < collSubTags.length; j++) {
-
-        //     // document.querySelector('.cat-page-content').appendChild(document.querySelector('.cat-page-card'))
-        //     var catPageCard = document.createElement('div');
-        //     catPageCard.className = 'cat-page-card';
-        //     document.querySelector('.cat-page-content').appendChild(catPageCard)
-        //     console.log(catPageCard)
-
-        // }
-
-    });
+                catPageCard.appendChild(catPageCardTitle);
+                catPageCard.appendChild(catPageCardBody);
+                catPageContent.appendChild(catPageCard);
+            }
+        });
+    }
 }
